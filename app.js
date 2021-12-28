00133a95b9bd40ec0bd7c9562e8e45a6154b2894
Phil <happy.melodist@gmail.com>
2021-12-28 11:54:25 -0600

    write the event listener that places the transcribed text from the chrome speech recognition api into the html element

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
 // https://caniuse.com/?search=speech%20recognition
 
+// const transcription = document.querySelector(".transcription");
+// let speechPara = document.createElement("p");
+
+// window.SpeechRecognition =
+//   window.SpeechRecognition || window.webkitSpeechRecognition;
+
+// const recognitionSpeech = new window.SpeechRecognition();
+// recognitionSpeech.interimResults = true;
+
+// recognitionSpeech.addEventListener("result", (e) => {
+//   const speechToText = Array.from(e.results)
+//     .map((result) => result[0])
+//     .map((result) => result.transcript)
+//     .join("");
+
+//   speechPara.innerText = speechToText;
+//   transcription.appendChild(speechPara);
+
+//   if (e.results[0].isFinal) {
+//     speechPara = document.createElement("p");
+//   }
+// });
+
+// recognitionSpeech.addEventListener("end", () => {
+//   recognitionSpeech.start();
+// });
+
+// recognitionSpeech.start();
+
+// be good and therefore good to yourself
+
+/*
+my code below
+*/
+
 const transcription = document.querySelector(".transcription");
 let speechPara = document.createElement("p");
 
@@ -17,6 +52,7 @@ recognitionSpeech.addEventListener("result", (e) => {
 
   speechPara.innerText = speechToText;
   transcription.appendChild(speechPara);
+  console.log(speechToText);
 
   if (e.results[0].isFinal) {
     speechPara = document.createElement("p");
